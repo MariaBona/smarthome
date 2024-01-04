b4c7b9d8247405c8971fe2229a01f355883edc11
Maria Bonarska <x21235201@student.ncirl.ie>
2024-01-04 21:49:38 +0000

    web-client boilerplate cont

--- client/controller/web-client/routes/index.js
+++ client/controller/web-client/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var grpc = require("@grpc/grpc-js")
+var protoLoader = require("@grpc/proto-loader")
+var PROTO_PATH = __dirname + "/../../protos/smarthome.proto"
+// load proto with enums as strings and keepCase to prevent removal of underscores
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {enums: String, keepCase: true})
+
+var smarthome_proto = grpc.loadPackageDefinition(packageDefinition).smarthome
+
+var client = new smarthome_proto.RegistryService("0.0.0.0:40000", grpc.credentials.createInsecure());
+var sub = new smarthome_proto.StatusService("0.0.0.0:40000", grpc.credentials.createInsecure());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
