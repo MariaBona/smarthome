2e5c0c199f161dbb2382e6ea1872974cbaee3d0a
Maria Bonarska <x21235201@student.ncirl.ie>
2024-01-06 18:40:44 +0000

    Refresh the page with correct light status

--- client/controller/web-client/routes/index.js
+++ client/controller/web-client/routes/index.js
@@ -16,17 +16,46 @@ var name = "Controller";
 var id = -1;
 var devices
 var lights = []
+var thermos = []
 var status_call
 var controller_status
+var pending_res
 
 /* Called when opening home page */
 router.get('/', function(req, res, next) {
   // helper function to render the index page in full
   function render(res) {
+    // update lights and thermostats lists with updated status
+    lights = []
+    thermos = []
+    for (const id of Object.keys(devices)) {
+      const type = devices[id].type;
+      if (type == "DEVICE_LIGHT") {
+        lights.push({
+          id: id,
+          name: devices[id].name,
+          type: type,
+          on: devices[id].status.on == 1 ? 'ON' : 'OFF'
+        })
+      } else if (type == "DEVICE_THERMOSTAT") {
+        thermos.push({
+          id: id,
+          name: devices[id].name,
+          type: type,
+          current_temp: devices[id].status.current_temp,
+          target_temp: devices[id].status.target_temp,
+          requesting_heat: devices[id].status.requesting_heat == 'false' ? 'NOT HEATING' : 'HEATING ON'
+        })
+      }
+    }
+
+    // render light and thermostat devices on the index page
     res.render('index', {
       title: "GRPC Smarthome",
       controller_status: controller_status,
-      lights: lights })
+      lights: lights,
+      thermos: thermos 
+    });
   }
   try {
     // get query keys (like: localhost:8080?light_id=1&on=0) -> [light_id, on]
@@ -65,23 +94,34 @@ router.get('/', function(req, res, next) {
               }
           });
 
-          // render light devices on the index page
-          for (const id of Object.keys(devices)) {
-            const type = devices[id].type;
-            if (type == "DEVICE_LIGHT") {
-              lights.push({
-                id: id,
-                name: devices[id].name,
-                type: type,
-                on: devices[id].status.on == 1 ? 'ON' : 'OFF'
-              })
-            }
+          // subscribe to all devices
+          //for (const id of Object.keys(devices)) 
+          {
+            var sub_call = sub.subscribe({id: 0});
+            sub_call.on('data', function(sub_response) {
+              console.log("sub_call data: ", sub_response);
+              // when subscribe returns response stream, update the device status
+              let dev = devices[sub_response.id]
+              dev.status = sub_response.status;
+              // render the page with updated statuses
+              if (pending_res) {
+                render(pending_res);
+              }
+            });
+
+            sub_call.on("error", function(e) {
+              console.log("Error occured on subscribe: " + e);
+            });
+
+            sub_call.on("end", function() {
+                console.log("Server closed the subscribe connection");
+            });
           }
+
           //res.render('index', { title: "GRPC Smarthome", controller_status: "Controller registered: " + response.new_name + ", id: " + response.id, lights: lights })
           render(res);
       });
     } else if ('light_id' in req.query) {
-      console.log('trigger light on or off')
       // query says this is a request to trigger the lights on or off
       let light_id = parseInt(req.query.light_id);
       let on = parseInt(req.query.on);
@@ -91,8 +131,12 @@ router.get('/', function(req, res, next) {
         on: on
       }, function(error, response) {
         console.log(response);
-        render(res);
+        // save he pending res to pending_res so we can render the page when subcribe response is received
+        pending_res = res
+        // if we try to render here, we will not have updated the light's status ON or OFF in the GUI
+        //render(res);
       });
+      //render(res);
       //res.render('index', { title: "GRPC Smarthome", controller_status: "Controller registered: " + response.new_name + ", id: " + response.id, lights: lights })
     } else {
       //res.render('index', { title: "GRPC Smarthome", controller_status: "Controller registered: " + response.new_name + ", id: " + response.id, lights: lights })
